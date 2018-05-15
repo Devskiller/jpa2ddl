89cbd72a263eca5000deae749672ac56d638fb45
Diego Díez Ricondo <diegodiez.ddr@gmail.com>
2018-05-15 18:34:19 +0200

    Process package level annotations (#20)
    * Process package level annotations

--- jpa2ddl-core/src/main/java/com/devskiller/jpa2ddl/SchemaGenerator.java
+++ jpa2ddl-core/src/main/java/com/devskiller/jpa2ddl/SchemaGenerator.java
@@ -64,6 +64,7 @@ class SchemaGenerator {
 
 		for (String packageName : settings.getPackages()) {
 			FileResolver.listClassNamesInPackage(packageName).forEach(metadata::addAnnotatedClassName);
+                	metadata.addPackage(packageName);
 		}
 
 		if (settings.getAction() != Action.UPDATE) {
