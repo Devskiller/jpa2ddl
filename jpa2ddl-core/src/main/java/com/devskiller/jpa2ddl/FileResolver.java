f3652bd9f4f95c5dfa1c02a95946f1ec3362bb17
pavel m <paullus325@gmail.com>
2020-12-08 11:53:57 +0300

    ZipFileSystem must be created before opening https://docs.oracle.com/javase/7/docs/technotes/guides/io/fsp/zipfilesystemprovider.html

--- jpa2ddl-core/src/main/java/com/devskiller/jpa2ddl/FileResolver.java
+++ jpa2ddl-core/src/main/java/com/devskiller/jpa2ddl/FileResolver.java
@@ -1,7 +1,6 @@
 package com.devskiller.jpa2ddl;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.FileSystems;
@@ -12,13 +11,14 @@ import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -80,6 +80,11 @@ class FileResolver {
 		PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:*.class");
 		while (resources.hasMoreElements()) {
 			URL resource = resources.nextElement();
+	         if ("jar".equals(resource.getProtocol())) {
+	            Map<String, String> env = new HashMap<>(); 
+	            env.put("create", "true");
+	            FileSystems.newFileSystem(resource.toURI(), env);
+	        }
 			Files.walkFileTree(Paths.get(resource.toURI()), new SimpleFileVisitor<Path>() {
 				@Override
 				public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
